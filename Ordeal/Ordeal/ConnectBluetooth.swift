f0ef1cafbdaa40b18bf2c5976ae3831a3a99e55d
Caio Piteli <caiogpiteli@gmail.com>
2023-10-25 09:32:17 -0300

    Bluetooth not connecting and unnmaed bluetooths founds

---
+++ Ordeal/Ordeal/ConnectBluetooth.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+import CoreBluetooth
+class BluetoothViewModel : NSObject, ObservableObject {
+  private var centralManager : CBCentralManager?
+  private var peripherals : [CBPeripheral] = []
+  @Published var peripheralNames: [String] = []
+  override init() {
+    super.init()
+    self.centralManager = CBCentralManager(delegate: self, queue: .main)
+  }
+  func conectando(name: CBPeripheral) {
+    centralManager?.connect(name)
+  }
+}
+extension BluetoothViewModel: CBCentralManagerDelegate {
+  func centralManagerDidUpdateState(_ central: CBCentralManager) {
+    if central.state == .poweredOn {
+      self.centralManager?.scanForPeripherals(withServices: nil)
+    }
+  }
+  func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+    if !peripherals.contains(peripheral) {
+      self.peripherals.append(peripheral)
+      self.peripheralNames.append(peripheral.name ?? "unnamed device")
+    }
+  }
+}
+struct ConnectBluetoothView: View {
+  @ObservedObject private var bluetoothViewModel = BluetoothViewModel()
+  var body: some View {
+    NavigationView {
+      List(bluetoothViewModel.peripheralNames, id: \.self) {peripheral in Text(peripheral)
+          .onTapGesture {
+            //bluetoothViewModel.conectando()
+          }
+        }.navigationTitle("Peripherals")
+    }
+  }
+}
+
+struct ConnectBluetoothView_Previews: PreviewProvider {
+  static var previews: some View {
+      ConnectBluetoothView()
+  }
+}
