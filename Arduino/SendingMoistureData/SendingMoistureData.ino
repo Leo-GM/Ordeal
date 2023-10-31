33de7fcd5ccd103dd4eb180370a80a95d4d27abc
Caio Piteli <caiogpiteli@gmail.com>
2023-10-31 11:32:24 -0300

    Retornando valor do arduino em porcentagem

--- Arduino/SendingMoistureData/SendingMoistureData.ino
+++ Arduino/SendingMoistureData/SendingMoistureData.ino
@@ -17,7 +17,7 @@ void setup() {
 }
 
 void loop() {
-  moisture = 0;
+moisture = 0;
 
 for (vezesMedida=0; vezesMedida<15; vezesMedida++){
   int medida = analogRead(A0);
@@ -29,10 +29,14 @@ for (interactions = 0; interactions<15; interactions++){
   moisture = moisture + dadosUmidade[interactions];
 }
 
-  moisture = moisture/15;
+moisture = moisture/15;
 
+ moisture = map(moisture, 1023, 180, 0, 100);  //Molhada (180) = 100% e Seco (1023) = 0%
  bluetooth.print(moisture);
 
- delay(3000);
+ delay(300);
 
 }
+
+//até 250 - molhadassa, nivel enxarcada (forma poça de agua na terra)
+//1023 - seco
